11542988f46f3dc3e0ab2b98042c87118a2a8a9d
c_laigle_catie <c.laigle@catie.fr>
2021-10-20 16:37:38 +0200

    Add Mbed OS version selection

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,21 +4,39 @@ import subprocess
 def git_init():
     return subprocess.check_call(["git", "init"])
 
+
 def git_add_all():
     return subprocess.check_call(["git", "add", "."])
 
+
 def git_commit():
     return subprocess.check_call(["git", "commit", "-m", "Initial commit"])
+
+
+def get_mbed_os_commit(version):
+    return subprocess.check_output(
+        ["git", "ls-remote", "https://github.com/ARMmbed/mbed-os.git", "-refs", version]
+    )
+
 
 def create_repository():
     git_init()
     git_add_all()
     git_commit()
 
+
 def main():
     if {{ cookiecutter.create_repository }}:
         create_repository()
 
+    if "{{ cookiecutter.mbed_os_version }}" != "latest":
+        commit = get_mbed_os_commit("{{ cookiecutter.mbed_os_version }}").decode()
+        if commit:
+            with open("mbed-os.lib", "a") as file:
+                file.write("#{}".format(commit.split("\t")[0]))
+        else:
+            print("[Warning] Mbed OS version does not exist")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
