f5cab0c2e88f1feb077602b4f4692f95e5f22dda
Bruno Perraudin <b.perraudin@catie.fr>
2021-10-22 16:13:34 +0200

    Add status message after project generation

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,10 @@
 import subprocess
 import os
 
+TERMINATOR = "\x1b[0m"
+WARNING = "\x1b[1;33m[WARNING]: "
+SUCCESS = "\x1b[1;32m[SUCCESS]: "
+
 
 def git_init():
     return subprocess.check_call(["git", "init"])
@@ -25,11 +29,15 @@ def remove_file(filepath):
 
 
 def main():
-    if {{ cookiecutter.create_repository }}:
+    if {{cookiecutter.create_repository}}:
         create_repository()
 
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
+
+    print(
+        SUCCESS + "Project generated in `{{ cookiecutter.project_slug }}`" + TERMINATOR
+    )
 
 
 if __name__ == "__main__":
